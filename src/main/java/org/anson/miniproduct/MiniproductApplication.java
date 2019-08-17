46b07321ef7828874b36d050adf945ce418e0b04
302824893@qq.com <sZHPtZJw9#9S34U>
2019-08-17 10:07:41 +0800

    Initial commit

---
+++ src/main/java/org/anson/miniproduct/MiniproductApplication.java
@@ -0,0 +1,13 @@
+package org.anson.miniproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniproductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniproductApplication.class, args);
+    }
+
+}
